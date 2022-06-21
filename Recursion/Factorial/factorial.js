c674fef485ce8a9824e02beed00f66d2d1ff41ef
Raman Kumar <rmn5124@gmail.com>
2022-06-21 19:15:59 +0530

    Create factorial.js

---
+++ Recursion/Factorial/factorial.js
@@ -0,0 +1,13 @@
+function fact(n){
+          if (n === 0)
+       return 1; 
+    else
+      return n * fact( n - 1 ); 
+    }
+class Solution{
+ 
+    factorial(n){
+        //code here
+      return fact(n);
+    }
+}
